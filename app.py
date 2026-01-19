5d144c246aa759c2da892828d7bf9f9ec72af2df
ekinetto7-ux <ekinetto7@gmail.com>
2026-01-19 23:20:54 +0900

    Update index route response message and run mode

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "アプリ起動確認OK"
+    return "起動成功！トップページです"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
